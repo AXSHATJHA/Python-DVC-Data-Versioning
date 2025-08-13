2fcc49d6b11f08e4a37f7bcce0bab0ac5ebe8216
Akshat <codedakshat@gmail.com>
2025-08-13 09:36:29 +0530

    Mark 1 - Adding data before DVC

---
+++ mycode.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+
+
+data = {'Name' : ['Alice', 'Charlie', 'Bob'],
+        'Age' : [18, 19, 28],
+        'City' : ['New York', 'Argentina', 'Arizona']
+        }
+
+df = pd.DataFrame(data)
+
+data_dir = 'data'
+os.makedirs(data_dir, exist_ok=True)
+
+file_path = os.path.join(data_dir, 'sample_data.csv')
+df.to_csv(file_path, index=False)
+
+print(f"File saved to path {file_path}")
